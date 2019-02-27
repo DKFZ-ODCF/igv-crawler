26f77a0da8afce42b1ae16e1abfea0a51a25b38c
Jules Kerssemakers <j.kerssemakers@dkfz-heidelberg.de>
2019-02-27 10:01:03 +0100

    tests: fix subject-grouping and keep each report individually, instead of repeatedly overwriting a single "demo" project.

--- test/test-crawl.sh
+++ test/test-crawl.sh
@@ -1,15 +1,15 @@
-time ../igv-crawler.pl --project demo --report full \
- --groupregex 'test/(te[sz]t)' \
+time ../igv-crawler.pl --project test-fullpath --report full \
+ --groupregex '(te[sz]t)' \
  --display="fullpath" \
  --scandir './data'
 
-time ../igv-crawler.pl --project demo --report full \
- --groupregex 'test/(te[sz]t)' \
+time ../igv-crawler.pl --project test-displayregex --report full \
+ --groupregex '(te[sz]t)' \
  --display="regex=^.+?/test/data/(.+)" \
  --scandir './data'
 
-time ../igv-crawler.pl --project demo --report full \
- --groupregex 'test/(te[sz]t)' \
+time ../igv-crawler.pl --project test-exclude --report full \
+ --groupregex '(te[sz]t)' \
  --display="regex=^.+?/test/data/(.+)" \
  --scandir './data' \
  --prunedir 'test-exclude'
